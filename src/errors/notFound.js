7c72809731007e7d1959db4b57cc450fdf572b82
nmishra <mishranarayan@hotmail.com>
2024-01-11 13:23:13 -0800

    Add features

---
+++ src/errors/notFound.js
@@ -0,0 +1,9 @@
+
+/**
+ * Express API "Not found" handler.
+ */
+function notFound(request, response, next) {
+  response.status(404).json({error: `Path not found: ${request.originalUrl}`});
+}
+
+module.exports = notFound;
