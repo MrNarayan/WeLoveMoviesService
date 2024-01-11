7c9000fd4d5e2ed20c8f752fc099ec6aa5fe4a51
nmishra <mishranarayan@hotmail.com>
2024-01-11 13:50:50 -0800

    no migration

--- knexfile.js
+++ knexfile.js
@@ -11,36 +11,18 @@ module.exports = {
     client: "postgresql",
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
   },
 
   production: {
     client: "postgresql",
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
   },
 
   test: {
     client: "sqlite3",
     connection: {
       filename: ":memory:",
-    },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
     },
     useNullAsDefault: true,
   },
