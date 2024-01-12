776d99aa72db50bf48cde4530c44af68942f3505
nmishra <mishranarayan@hotmail.com>
2024-01-11 23:56:53 -0800

    enable migration

--- knexfile.js
+++ knexfile.js
@@ -11,12 +11,24 @@ module.exports = {
     client: "postgresql",
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   production: {
     client: "postgresql",
     connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   test: {
